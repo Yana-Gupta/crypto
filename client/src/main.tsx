b2d9c1a8d792ae9fb5631d4053d19f901539da36
Yana Gupta <yanagupta897@gmail.com>
2023-07-09 19:09:32 +0530

    changed some part

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { TransactionProvider } from './context/TransactionContext.tsx';
 
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TransactionProvider>
     <React.StrictMode>
